6dae32afc59fdaf5bb9416a73207e340acd43938
AbhishekWadhai <abhiabhi9488@gmail.com>
2026-01-21 14:48:54 +0530

    with new flutter update

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.axiomos.jkumarsafetysure"
+    compileSdk = 36
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.axiomos.jkumarsafetysure"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
+apply(plugin = "com.google.gms.google-services")
---
+++ android/app/src/main/kotlin/com/example/sure_safe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.axiomos.jkumarsafetysure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
